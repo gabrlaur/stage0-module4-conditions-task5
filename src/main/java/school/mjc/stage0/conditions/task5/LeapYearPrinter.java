8ce20327e6b7b40e13c57c1162f03a9f11f13b76
gabrlaur <45432522+gabrlaur@users.noreply.github.com>
2022-08-05 22:05:37 +0300

    Task #5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int remainder = year % 4;
+        switch (remainder) {
+            case 0:
+                System.out.println("leap");
+                break;
+            default:
+                System.out.println("not leap");
+                break;
+        }
     }
 }
